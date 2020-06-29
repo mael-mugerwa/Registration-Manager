6b74030d10d06277888b35fc37abcfefa48e5ec1
mael-mugerwa <mael.mugerwa@mail.mcgill.ca>
2020-06-29 15:01:27 -0400

    gradle build finally succesful

--- Backend/src/main/java/eventregistration/EventRegistrationApplication.java
+++ Backend/src/main/java/eventregistration/EventRegistrationApplication.java
@@ -1,3 +1,5 @@
+package eventregistration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
--- Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
+++ Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
@@ -1,13 +1,16 @@
 package eventregistration;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
-class EventRegistrationApplicationTests {
+public class EventRegistrationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		int a = 1;
+		assertEquals(1, a);
 	}
 
 }
