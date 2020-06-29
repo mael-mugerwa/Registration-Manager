3fe1c0d55b797abe5d9c8202fdbc9fb71acd2ef0
mael-mugerwa <mael.mugerwa@mail.mcgill.ca>
2020-06-29 11:08:48 -0400

    fixed multiple gradle files

--- Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
+++ Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
@@ -2,7 +2,6 @@ package eventregistration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertNull;
 
 @SpringBootTest
 class EventRegistrationApplicationTests {
