d5bc0333287c3a477f8565d00410a8a83485ce86
mael-mugerwa <mael.mugerwa@mail.mcgill.ca>
2020-06-29 16:11:59 -0400

    testing with computation application

--- Backend/src/main/java/eventregistration/EventRegistrationApplication.java
+++ Backend/src/main/java/eventregistration/EventRegistrationApplication.java
@@ -4,17 +4,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import view.ComputationPage;
 
 @RestController
 @SpringBootApplication
 public class EventRegistrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventRegistrationApplication.class, args);
+		//SpringApplication.run(EventRegistrationApplication.class, args);
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ComputationPage().setVisible(true);
+            }
+        });
 	}
-
+/*
   @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
   }
+  */
 }
