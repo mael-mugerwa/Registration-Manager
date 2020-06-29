20978344569c7fd91a71022a26e3c0835641bc3a
mael-mugerwa <mael.mugerwa@mail.mcgill.ca>
2020-06-29 10:13:46 -0400

    renaming backend folder

--- Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
+++ Backend/src/test/java/eventregistration/EventRegistrationApplicationTests.java
@@ -2,6 +2,7 @@ package eventregistration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertNull;
 
 @SpringBootTest
 class EventRegistrationApplicationTests {
