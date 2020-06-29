41d06462241a273d90cb780a358bd19bbdae58c0
mael-mugerwa <mael.mugerwa@mail.mcgill.ca>
2020-06-29 12:05:52 -0400

    Initial commit of the backend application

--- Backend/src/main/java/eventregistration/EventRegistrationApplication.java
+++ Backend/src/main/java/eventregistration/EventRegistrationApplication.java
@@ -1,8 +1,9 @@
-package eventregistration;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class EventRegistrationApplication {
 
@@ -10,4 +11,8 @@ public class EventRegistrationApplication {
 		SpringApplication.run(EventRegistrationApplication.class, args);
 	}
 
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 }
